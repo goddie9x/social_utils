138290f7d3cc982ed344680a8e864f6b77cd6f27
Hoang Minh Tam <goddie9x@yahoo.com>
2024-08-23 21:28:13 +0700

    feat: add GEN_USER_PROFILE_ROUTE

--- constants/clientRoute.js
+++ constants/clientRoute.js
@@ -1,4 +1,5 @@
 const CLIENT_ROUTES = {
     GEN_ROUTE_BY_ID: (path, id) => path + id,
-    GEN_FRIEND_REQUEST_LIST_ROUTE: (id) => GEN_ROUTE_BY_ID('/friends/request/', id)
+    GEN_FRIEND_REQUEST_LIST_ROUTE: (id) => GEN_ROUTE_BY_ID('/friends/request/', id),
+    GEN_USER_PROFILE_ROUTE:(id)=>GEN_ROUTE_BY_ID('/users/profile/',id),
 }
