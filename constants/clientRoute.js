c7408bb4df64f04cc105338ab46e6da1b568069e
Hoang Minh Tam <goddie9x@yahoo.com>
2024-08-23 21:35:50 +0700

    fix: export CLIENT_ROUTES

--- constants/clientRoute.js
+++ constants/clientRoute.js
@@ -3,3 +3,5 @@ const CLIENT_ROUTES = {
     GEN_FRIEND_REQUEST_LIST_ROUTE: (id) => GEN_ROUTE_BY_ID('/friends/request/', id),
     GEN_USER_PROFILE_ROUTE:(id)=>GEN_ROUTE_BY_ID('/users/profile/',id),
 }
+
+module.exports = CLIENT_ROUTES;
