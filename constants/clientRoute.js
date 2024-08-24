f2743e46d2cda852d97aea1301cb51b2196ad153
Hoang Minh Tam <goddie9x@yahoo.com>
2024-08-24 17:40:14 +0700

    fix: add this.GEN_ROUTE_BY_ID call

--- constants/clientRoute.js
+++ constants/clientRoute.js
@@ -1,7 +1,7 @@
 const CLIENT_ROUTES = {
     GEN_ROUTE_BY_ID: (path, id) => path + id,
-    GEN_FRIEND_REQUEST_LIST_ROUTE: (id) => GEN_ROUTE_BY_ID('/friends/request/', id),
-    GEN_USER_PROFILE_ROUTE:(id)=>GEN_ROUTE_BY_ID('/users/profile/',id),
+    GEN_FRIEND_REQUEST_LIST_ROUTE: (id) => this.GEN_ROUTE_BY_ID('/friends/request/', id),
+    GEN_USER_PROFILE_ROUTE:(id)=>this.GEN_ROUTE_BY_ID('/users/profile/',id),
 }
 
 module.exports = CLIENT_ROUTES;
