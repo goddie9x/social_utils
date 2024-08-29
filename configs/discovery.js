1d98079f6a27571ef2f57c837c7d7361bf7abc22
Hoang Minh Tam <goddie9x@yahoo.com>
2024-08-29 20:29:42 +0700

    feat: add connect to eureka discovery server

---
+++ configs/discovery.js
@@ -0,0 +1,37 @@
+const { Eureka } = require('eureka-js-client');
+
+const connectToDiscoveryServer = () => {
+    const port = process.env.PORT || 3000;
+
+    const client = new Eureka({
+        instance: {
+            app: 'user-service',
+            hostName: 'localhost',
+            ipAddr: '127.0.0.1',
+            statusPageUrl: 'http://localhost:' + port + '/info',
+            port: {
+                '$': port,
+                '@enabled': true,
+            },
+            vipAddress: 'user-service',
+            dataCenterInfo: {
+                '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+                name: 'MyOwn',
+            },
+        },
+        eureka: {
+            host: process.env.EUREKA_DISCOVERY_SERVER_HOST || 'localhost',
+            port: process.env.EUREKA_DISCOVERY_SERVER_PORT || 8761,
+        },
+    });
+
+    client.start(error => {
+        if (error) {
+            console.log('Error starting Eureka client:', error);
+        } else {
+            console.log('Eureka client started');
+        }
+    });
+}
+
+module.exports = connectToDiscoveryServer;
