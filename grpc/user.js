b1b7daa653fc1b2d5688ad9c831afdf3a462937e
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-05 13:04:43 +0700

    refactor: remove format grpc user response and add extract user response for reuse in other service

--- grpc/user.js
+++ grpc/user.js
@@ -1,9 +1,68 @@
+const extractUserResponse = (response) => {
+    const user = {
+        id: response.getId(),
+        username: response.getUsername(),
+        firstName: response.getFirstname(),
+        lastName: response.getLastname(),
+        avatarUrl: response.getAvatarurl(),
+        coverPhotoUrl: response.getCoverphotourl(),
+        role: response.getRole(),
+        createdAt: response.getCreatedat(),
+        updatedAt: response.getUpdatedat(),
+        emails: response.getEmailsList().map(email => ({
+            email: email.getEmail(),
+            isPrimary: email.getIsprimary(),
+            isVerified: email.getIsverified()
+        })),
+        phones: response.getPhonesList().map(phone => ({
+            number: phone.getNumber(),
+            isPrimary: phone.getIsprimary(),
+            isVerified: phone.getIsverified()
+        })),
+        followerIds: response.getFollowerIdsList()
+    };
+    return user;
+};
+
+
+const extractGetUsersWithPaginationResponse = (response) => {
+    const result = {
+        page: response.getPage(),
+        limit: response.getLimit(),
+        totalUsers: response.getTotalUsers(),
+        totalPages: response.getTotalPages(),
+        users: response.getUsersList().map(extractUserResponse),
+    };
+    return result;
+};
+const extractDeleteResponse = (response) => {
+    return {};
+};
+
+const extractGetListUserByIdsResponse = (response) => {
+    const result = {
+        users: response.getUsersList().map(extractUserResponse),
+    };
+    return result;
+};
+
+const extractAuthResponse = (response) => {
+    return response.getToken();
+};
+
+const extractDeleteMultipleResponse = (response) => {
+    const result = {
+        deletedCount: response.getDeletedCount()
+    };
+    return result;
+};
+
 const getUserByIdGRPCGenerate = ({ userMessages, userServiceClient }) => {
     return (userId) => new Promise((resolve, reject) => {
         const userIdString = userId.toString();
         const request = new userMessages.GetUserByIdRequest();
-        request.setId(userIdString);
 
+        request.setId(userIdString);
         userServiceClient.getUserById(request, (error, response) => {
             if (error) {
                 return reject(error);
@@ -31,111 +90,7 @@ const getListUserByIdsGRPCGenerate = ({ userMessages, userServiceClient }) => {
     })
 }
 
-const formatGetUsersWithPaginationResponse = (result, userMessages) => {
-    const response = new userMessages.GetUsersWithPaginationResponse();
-    response.setPage(result.page);
-    response.setLimit(result.limit);
-    response.setTotalUsers(result.totalUsers);
-    response.setTotalPages(result.totalPages);
-
-    result.users.forEach(user => {
-        const userMessage = formatUserResponse(user,userMessages);
-
-        response.addUsers(userMessage);
-    });
-    return response;
-};
-const formatGetListUserByIdsResponse = (result, userMessages) => {
-    const response = new userMessages.GetListUserByIdsResponse();
-
-    result.users.forEach(user => {
-        const userMessage = new userMessages.User();
-        userMessage.setId(user.id);
-        userMessage.setUsername(user.username);
-        userMessage.setFirstname(user.firstName);
-        userMessage.setLastname(user.lastName);
-        userMessage.setAvatarurl(user.avatarUrl);
-        userMessage.setCoverphotourl(user.coverPhotoUrl);
-        userMessage.setRole(user.role);
-        userMessage.setCreatedat(user.createdAt);
-        userMessage.setUpdatedat(user.updatedAt);
-        user.emails?.forEach(email => {
-            const emailMessage = new userMessages.Email();
-            emailMessage.setEmail(email.email);
-            emailMessage.setIsprimary(email.isPrimary);
-            emailMessage.setIsverified(email.isVerified);
-            userMessage.addEmails(emailMessage);
-        });
-        user.phones?.forEach(phone => {
-            const phoneMessage = new userMessages.Phone();
-            phoneMessage.setNumber(phone.number);
-            phoneMessage.setIsprimary(phone.isPrimary);
-            phoneMessage.setIsverified(phone.isVerified);
-            userMessage.addPhones(phoneMessage);
-        });
-        user.followerIds?.forEach(followerId => {
-            userMessage.addFollowerIds(followerId);
-        });
-
-        response.addUsers(userMessage);
-    });
-    return response
-}
-
-const formatAuthResponse = (result, userMessages) => {
-    const response = new userMessages.AuthTokenResponse();
-    response.setToken(result);
-    return response;
-};
-
-const formatUserResponse = (result, userMessages) => {
-    const response = new userMessages.User();
-    response.setId(result.id);
-    response.setUsername(result.username);
-    response.setFirstname(result.firstName);
-    response.setLastname(result.lastName);
-    response.setAvatarurl(result.avatarUrl);
-    response.setCoverphotourl(result.coverPhotoUrl);
-    response.setRole(result.role);
-    response.setCreatedat(result.createdAt);
-    response.setUpdatedat(result.updatedAt);
-    result.emails?.forEach(email => {
-        const emailMessage = new userMessages.Email();
-        emailMessage.setEmail(email.email);
-        emailMessage.setIsprimary(email.isPrimary);
-        emailMessage.setIsverified(email.isVerified);
-        response.addEmails(emailMessage);
-    });
-    result.phones?.forEach(phone => {
-        const phoneMessage = new userMessages.Phone();
-        phoneMessage.setNumber(phone.number);
-        phoneMessage.setIsprimary(phone.isPrimary);
-        phoneMessage.setIsverified(phone.isVerified);
-        response.addPhones(phoneMessage);
-    });
-    result.followerIds?.forEach(followerId => {
-        response.addFollowerIds(followerId);
-    });
-    return response;
-};
-
-const formatDeleteResponse = () => {
-    return new userMessages.Empty();
-};
-
-const formatDeleteMultipleResponse = (result, userMessages) => {
-    const response = new userMessages.DeleteMultipleUsersResponse();
-    response.setDeletedCount(result.deletedCount);
-    return response;
-};
-
 module.exports = {
     getUserByIdGRPCGenerate,
-    getListUserByIdsGRPCGenerate,
-    formatAuthResponse,
-    formatDeleteMultipleResponse,
-    formatDeleteResponse,
-    formatGetUsersWithPaginationResponse,
-    formatUserResponse,
-    formatGetListUserByIdsResponse,
-};
+    getListUserByIdsGRPCGenerate
+}
