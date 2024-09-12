9835b0021b1b6044feef7195103f633676301aed
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-12 16:40:06 +0700

    feat: add batch auto pull utils for new update

---
+++ pull_request_for_multipath.bat
@@ -0,0 +1,19 @@
+@echo on
+setlocal
+
+rem Save the original directory
+set original_dir=%cd%
+
+set dirs=..\..\friend_service\utils ..\..\message_sevice\utils ..\..\notification_service\utils ..\..\socket_gateway\utils ..\..\user_service\utils
+
+set cmd=git pull origin main
+
+rem Loop through directories
+for %%d in (%dirs%) do (
+    echo Running command in %%d
+    pushd %%d
+    %cmd%
+    popd
+)
+
+echo Back to original directory: %original_dir%
