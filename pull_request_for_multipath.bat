c5b634bef6a0f02f83d9d6f073eb1c9906ce86df
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-14 14:57:04 +0700

    fix: correct name of notification_service

--- pull_request_for_multipath.bat
+++ pull_request_for_multipath.bat
@@ -4,7 +4,7 @@ setlocal
 rem Save the original directory
 set original_dir=%cd%
 
-set dirs=..\..\friend_service\utils ..\..\message_sevice\utils ..\..\notification_service\utils ..\..\socket_gateway\utils ..\..\user_service\utils
+set dirs=..\..\friend_service\utils ..\..\message_service\utils ..\..\notification_service\utils ..\..\socket_gateway\utils ..\..\user_service\utils
 
 set cmd=git pull origin main
 
