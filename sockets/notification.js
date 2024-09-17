04cf652d43e97bf129f6d7d347d650e2ebbe9291
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-17 16:32:48 +0700

    fix: misleading between message and notification channel socket

--- sockets/notification.js
+++ sockets/notification.js
@@ -1,22 +1,22 @@
 const { NOTIFICATION_CHANNEL } = require('../constants/socketChannel');
 const authSocketMiddleware = require('../middlewares/authSocketMiddleware');
-const {mappingSocketBuilder} = require('.');
+const { mappingSocketBuilder } = require('.');
 
 const notificationSocketBuilder = (io) => {
     mappingSocketBuilder({
         io,
-        middlewares:[authSocketMiddleware],
-        namespace:NOTIFICATION_CHANNEL.NAMESPACE,
-        builder:(namespace)=>{
+        middlewares: [authSocketMiddleware],
+        namespace: NOTIFICATION_CHANNEL.NAMESPACE,
+        builder: (namespace) => {
             namespace.on('connection', socket => {
                 const currentUser = socket.currentUser;
-        
+
                 console.log(`User connected to ${NOTIFICATION_CHANNEL.NAMESPACE}: ${currentUser.username}`);
-                socket.join('new-message-' + currentUser.userId);
-        
+                socket.join(NOTIFICATION_CHANNEL.EVENTS.NEW_NOTIFICATION + '-' + currentUser.userId);
+
                 socket.on('disconnect', () => {
                     console.log(`User disconnected from ${NOTIFICATION_CHANNEL.NAMESPACE}: ${socket.currentUser.username}`);
-                    socket.leave('new-message-' + currentUser.userId);
+                    socket.leave(NOTIFICATION_CHANNEL.EVENTS.NEW_NOTIFICATION + '-' + currentUser.userId);
                 });
             });
         }
