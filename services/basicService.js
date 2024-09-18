59bb7eb4d2570d089555b7940ec7f25cec7d607b
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-18 17:02:45 +0700

    refactor: add sort options

--- services/basicService.js
+++ services/basicService.js
@@ -1,10 +1,13 @@
 class BasicService {
-    async getPaginatedResults({ model, query, page, limit }) {
+    async getPaginatedResults({ model, query, page = 1, limit = 10, sort = {
+        createdAt: -1
+    } }) {
         const skip = (page - 1) * limit;
         const getResultsPromise = model.find(query)
             .sort({ createdAt: -1 })
             .skip(skip)
-            .limit(parseInt(limit));
+            .limit(parseInt(limit))
+            .sort(sort);
         const getTotalDocumentsPromise = model.countDocuments(query);
         const [results, totalDocuments] = await Promise.all([getResultsPromise, getTotalDocumentsPromise]);
 
