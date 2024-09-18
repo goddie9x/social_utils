c69502b639c4eb4ea8863ba3e73313b6a5b67a04
Hoang Minh Tam <goddie9x@yahoo.com>
2024-09-18 10:13:44 +0700

    feat: add message socket event constants

--- constants/socketChannel.js
+++ constants/socketChannel.js
@@ -1,15 +1,17 @@
 const CHANNELS = {
-    MESSAGE_CHANNEL:{
-        NAMESPACE:'message',
-        EVENTS:{
-            NEW_MESSAGE:'new-message'
-        }
+    MESSAGE_CHANNEL: {
+        NAMESPACE: 'message',
+        EVENTS: {
+            NEW_MESSAGE: 'new-message',
+            HIDE_MESSAGE: 'hide-message',
+            UPDATE_GROUP_CHAT_INFO: 'update-group-chat-information',
+        },
     },
-    NOTIFICATION_CHANNEL:{
-        NAMESPACE:'notification',
-        EVENTS:{
-            NEW_NOTIFICATION:'new-notification'
-        }
+    NOTIFICATION_CHANNEL: {
+        NAMESPACE: 'notification',
+        EVENTS: {
+            NEW_NOTIFICATION: 'new-notification',
+        },
     },
 };
 
