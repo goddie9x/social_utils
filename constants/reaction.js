a6a5de64d71442e1e46995e51c330dfc7636d643
Hoang Minh Tam <goddie9x@yahoo.com>
2024-10-15 17:34:05 +0700

    feat: add constants of reaction

---
+++ constants/reaction.js
@@ -0,0 +1,17 @@
+const REACTION = {
+    TARGET_TYPE: {
+        POST: 0,
+        COMMENT: 1,
+        MESSAGE: 2,
+    },
+    TYPE: {
+        LIKE: 0,
+        LOL: 1,
+        SMILE: 2,
+        SURPRISE: 3,
+        SAD: 4,
+        HATE: 5,
+    }
+};
+
+module.exports = REACTION;
